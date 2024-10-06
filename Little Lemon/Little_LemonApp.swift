63ee3cf8a02065bffc40ae6f8e775560bd32fa0e
OxADD1 <119510746+OxADD1@users.noreply.github.com>
2024-10-06 14:27:03 +0200

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Adrian Eberhardt on 06.10.24.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
