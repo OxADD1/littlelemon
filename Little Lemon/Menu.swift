5a844e2dc2062c2555c17e721789475decb9e70e
Adrian <eberhardt.adrian@yahoo.com>
2024-10-12 11:44:58 +0200

    Set up the navigation

---
+++ Little Lemon/Menu.swift
@@ -0,0 +1,25 @@
+//
+//  Menu.swift
+//  Little Lemon
+//
+//  Created by Adrian Eberhardt on 12.10.24.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        VStack{
+            Text("Little Lemon Restaurant")
+            Text("Albstadt")
+            Text("Beste Restaurant in der Stadt")
+            List{
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    Menu()
+}
