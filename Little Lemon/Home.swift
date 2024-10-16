7aecfd651d30809ecab9a6bac5e512019e880509
Adrian <eberhardt.adrian@yahoo.com>
2024-10-16 11:03:56 +0200

    step 4

--- Little Lemon/Home.swift
+++ Little Lemon/Home.swift
@@ -1,12 +1,17 @@
 import SwiftUI
 
 struct Home: View {
+    // Füge den PersistenceController hinzu
+    let persistence = PersistenceController.shared
+    
     var body: some View {
         TabView {
             Menu()
                 .tabItem {
                     Label("Menu", systemImage: "list.dash")
                 }
+                // Füge den managedObjectContext hier hinzu
+                .environment(\.managedObjectContext, persistence.container.viewContext)
             
             UserProfile()
                 .tabItem {
